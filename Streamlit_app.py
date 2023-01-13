e0815c960493aa705be47e8c187f27f8b6edbc40
Swarna <122525753+SwarnaGitHub12@users.noreply.github.com>
2023-01-13 12:48:02 +0530

    Import pandas

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -5,7 +5,9 @@ streamlit.header('Breakfasrlt Favorites')
 streamlit.text('🍵Omega 3 & blueberry oatmeal')
 streamlit.text('🌮kale,spinach & rocket smoothie')
 streamlit.text('🥚Hard boiled free range egg')
-streamlit.header('🍌🍓 Build your own fruit smoothie 🫐🥝')
+streamlit.text('🥑🍞 avacado toast')
+
+streamlit.header('🍌🍓 Build your own fruit smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
