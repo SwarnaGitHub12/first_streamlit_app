3639c7ba91162656f216d691942e45ede1411254
Swarna <122525753+SwarnaGitHub12@users.noreply.github.com>
2023-01-24 16:27:56 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -56,7 +56,7 @@ except URLError as e:
 
 
 
-streamlit.text("The fruite load list contains:")
+streamlit.header("The fruite load list contains:")
 
 def get_fruit_load_list():
       with my_cnx.cursor() as my_cur:
@@ -73,4 +73,4 @@ def get_fruit_load_list():
 fruit_choice1 = streamlit.text_input('What fruit would you like to add')
 
 streamlit.write('Thanks for adding ', fruit_choice1)
-#my_cur.execute("insert into fruit_load_list values('from streamlit')")
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
