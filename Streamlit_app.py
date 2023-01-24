f8776ff0d281260b168a2db8870d65b4ef97fb4b
Swarna <122525753+SwarnaGitHub12@users.noreply.github.com>
2023-01-24 12:44:27 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -42,7 +42,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 
 my_cur = my_cnx.cursor()
 
-my_cur.execute("SELECT *from fruit_load_list")
+my_cur.execute("SELECT * from fruit_load_list")
 
 my_data_row = my_cur.fetchone()
 
